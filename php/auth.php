70e204af89ec018dc71f27aaff71a18526a360a9
vmsou <viniciusmarcs@icloud.com>
2023-06-09 19:57:51 -0300

    fix: JS now reads Auth PHP result

--- php/auth.php
+++ php/auth.php
@@ -1,17 +1,23 @@
 <?php
     include "db-connection.php";
+    session_start();
     
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-    $hash = password_hash($password, PASSWORD_DEFAULT);
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        $username = mysqli_real_escape_string($db, $_POST["username"]);
+        $password = mysqli_real_escape_string($db, $_POST["password"]);
+        $hash = password_hash($password, PASSWORD_DEFAULT);
 
-    $stmt = $db->prepare("SELECT username, password FROM tbUser WHERE username = '?' AND password = '?'");
-    $stmt->execute([$username, $hash]);
-    $user = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    if (isset($user) && !empty($user)) {
-        echo TRUE;
-    } else {
-        echo FALSE;
+        $stmt = "SELECT * FROM tbUser WHERE username = '$username' AND password = '$hash'";
+        $result = $db->query($stmt);
+        $count = mysqli_num_rows($result);
+        if ($count == 1) {
+            session_regenerate_id();
+            $row = mysqli_fetch_array($result);
+            $_SESSION["username"] = $row["username"];
+            echo "Sucesso";
+        } else {
+            echo "Falhou";
+        }
     }
+    
 ?>
